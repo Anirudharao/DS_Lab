a3b6daaf03c64792b8f28dfc48c6e3ba164b6260
Anirudharao <77189479+Anirudharao@users.noreply.github.com>
2021-12-10 00:47:30 +0530

    Add files via upload

--- Lab3Prg1.cpp
+++ Lab3Prg1.cpp
@@ -1,91 +1,54 @@
 #include<iostream>
 using namespace std;
-class transposeM{
-    int m1[20][20],m2[20][20],i,j,row,column,t;
-public:
-    void read(){
-        t=0;
-        cout<<"Enter the number of row: \n";
-        cin>>row;
-        cout<<"enter the number of column: \n";
-        cin>>column;
 
-        for(i=0;i<row;i++){
-            for(j=0;j<column;j++){
-                cin>>m1[i][j];
+class queue{
+    int a[10];
+    int size;
+    int front;
+    int rear;
 
-                if(m1[i][j]){
-                    t++;
-                            m2[t][0]=i+1;
-                            m2[t][1]=j+1;
-                            m2[t][2]=m1[i][j];
-
-                }
-
-
-            }
-        }
-
-        m2[0][0]=row;
-        m2[0][1]=column;
-        m2[0][2]=t;
+    queue(int n){
+        size = n;
+        front = -1;
+        rear = -1;
     }
 
-    void displaysp(){
-cout<<"sparse matrix is: \n";
-        for(i=0;i<=t;i++){
-            for(j=0;j<3;j++){
-                cout<<m2[i][j]<<" ";
-            }
-            cout<<"\n";
-        }
-    }
-
-    void transpose(){
-        int transpose[20][3];
-
-                        transpose[0][0]=m2[0][1];
-                        transpose[0][1]=m2[0][0];
-                        transpose[0][2]=m2[0][2];
-        cout<<"Transpose is: \n";
-        int q=1;
-        for(i=1;i<=column;i++){
-            for(int p=1;p<=t;p++){
-                if(m2[p][2]==i){
-                    transpose[q][0]=m2[p][1];
-                    transpose[q][1]=m2[p][0];
-                    transpose[q][2]=m2[p][2];
-                    q++;
-
-                }
-            }
-        }
-
-        for(i=0;i<=column;i++){
-            for(j=0;j<3;j++){
-                cout<<transpose[i][j]<<" ";
-            }
-            cout<<"\n";
-        }
-
-    }
-    void display(){
-        for(i=0;i<row;i++){
-                for(j=0;j<column;j++){
-                    cout<<m1[i][j]<<" ";
-
-
-                }
-                cout<<"\n";
-            }
-
-    }
+    void insert(int);
+    int remove();
+    void display();
 };
-int main(int argc,char ** argv){
-    transposeM obj;
-    obj.read();
-    obj.display();
-    obj.displaysp();
-    obj.transpose();
+
+void queue::insert(int ele){
+    if(rear == size-1){
+        cout<<"Queue is full"<<endl;
+    }
+    else{
+        a[++rear] = ele;
+    }
+}
+
+int queue::remove(){
+    if(rear == -1){
+        cout<<"Queue is empty"<<endl;
+    }
+    else{
+        return a[++front];
+    }
+}
+
+void queue::display(){
+    if(rear == front){
+        cout<<"Queue is empty"<<endl;
+    }
+    else{
+        for(int i=front; i<=rear; i++){
+        cout<<a[i]<<" ";
+        }
+    }
+    
+}
+
+int main(){
+    
     return 0;
 }
